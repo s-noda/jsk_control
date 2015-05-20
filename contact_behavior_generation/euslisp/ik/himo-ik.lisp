0f8e7db03b44665002b770284ebab4b01dba1c10
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-05-19 19:21:23 -0700

    rough inmp of himo ik

--- contact_behavior_generation/euslisp/ik/himo-ik.lisp
+++ contact_behavior_generation/euslisp/ik/himo-ik.lisp
@@ -1,5 +1,10 @@
 #!/usr/bin/env roseus
 
+(cond
+ ((not (and (boundp '*robot*) *robot*))
+  (require "package://hrpsys_ros_bridge_tutorials/models/hrp2jsknts.l")
+  (setq *robot* (instance hrp2jsknts-robot :init))))
+
 (defvar *elbow-end-coords*
   (mapcar
    #'(lambda (k name)
@@ -53,7 +58,7 @@
       (cond
        ((not (and (boundp '*viewer*) *viewer*))
         (objects (list *robot*))))
-      (send *robot* :reset-pose)
+      (send *robot* :reset-manip-pose)
       (send *robot* :newcoords (make-coords))
       (send-all (send *robot* :links) :worldcoords)
       (send *viewer* :draw-objects)))
@@ -69,6 +74,7 @@
    (link-list
     (mapcar #'(lambda (mt) (send *robot* :link-list (send mt :parent)))
 	    move-target))
+   (union-link-list (union (flatten link-list) nil))
    (target-coords (send-all move-target :copy-worldcoords))
    (translation-axis (make-list (length move-target) :initial-element t))
    (rotation-axis
@@ -85,28 +91,77 @@
    (cnt -1)
    (debug-view :no-message)
    ;;
-   (best-rsd nil)
-   (now-rsd nil)
    collision-avoidance-link-pair
    (min #F(-1000 -1000 -1000 -500 -500 -500))
    (max #F(1000 1000 1000 500 500 500))
    ret
+   ;;
+   (rope-initial-points
+    (list (v+ (float-vector 0 -500 500)  (send (send *robot* :get :rarm-elbow-cascoords) :worldpos))
+          (v+ (float-vector 300 0 0)  (send (send *robot* :get :rarm-elbow-cascoords) :worldpos))
+          (v+ (float-vector -300 0 0)  (send (send *robot* :get :larm-elbow-cascoords) :worldpos))
+          (v+ (float-vector 0 500 500)  (send (send *robot* :get :larm-elbow-cascoords) :worldpos))
+          ))
+   ;;
+   (mt2rope-scale 1e-3)
+   (rope2mt-scale 1e-2)
+   (rope2length-scale 1e-3)
    &allow-other-keys)
+  (setq *ik-convergence-user-check* nil)
   (setq
    ret
    (catch :ik-stop
-     (send* *robot* :fullbody-inverse-kinematics
+     (send* *robot* :inverse-kinematics
 	    target-coords
 	    :move-target move-target
 	    :link-list link-list
+            :union-link-list union-link-list
 	    :collision-avoidance-link-pair
 	    collision-avoidance-link-pair
 	    :null-space
 	    #'(lambda nil
+                (send-all union-link-list :put :move-angle 0)
 		(x::window-main-one)
 		(incf cnt)
+                (mapcar
+                 #'(lambda (pos1 pos2)
+                     (send *viewer* :viewsurface :3d-line pos1 pos2))
+                 (cdr rope-initial-points) rope-initial-points)
+                ;;
+                (let* ((rope-points (copy-seq rope-initial-points))
+                       buf)
+                  (mapcar
+                   #'(lambda (mt ll pos pos-buf)
+                       (map cons
+                            #'(lambda (l dif) (send l :put :move-angle
+                                                    (+ (send l :get :move-angle) dif)))
+                            ll
+                            (scale mt2rope-scale
+                                   (transform
+                                    (transpose
+                                     (send *robot* :calc-jacobian-from-link-list ll
+                                           :translation-axis '(t) :rotation-axis '(t) :move-target mt))
+                                    (concatenate float-vector
+                                                 (send (make-coords :pos pos) :difference-position mt)
+                                                 (float-vector 0 0 0)))))
+                       ;;
+                       (setq buf (send (make-coords :pos pos) :difference-position mt))
+                       ;; (send mt :difference-position (make-coords :pos pos)))
+                       (dotimes (i 3)
+                         (setf (aref pos-buf i)
+                               (+ (aref pos-buf i) (* rope2mt-scale (aref buf i)))))
+                       )
+                   move-target link-list
+                   (subseq rope-initial-points 1 (- (length rope-initial-points) 1))
+                   (subseq rope-points 1 (- (length rope-points) 1)))
+                  ;;
+                  (mapcar
+                   #'(lambda (from to) (dotimes (i 3) (setf (aref to i) (aref from i))))
+                   rope-points rope-initial-points)
+                  )
+                ;;
+                (print (coerce (send-all union-link-list :get :move-angle) float-vector))
                 )
-	    :target-centroid-pos target-centroid-pos
 	    :min min :max max
 	    :avoid-weight-gain avoid-weight-gain
 	    :translation-axis translation-axis
@@ -119,6 +174,7 @@
 	    args
 	    )))
   (format t "    time: ~A~%" (send tm :stop))
+  (setq *ik-convergence-user-check* t)
   ret
   )
 
