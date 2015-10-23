c6eb47cd3df592ed96b31d3ee9ae6fbaff3c8ff4
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-10-23 21:35:00 +0900

    update parameters

--- contact_behavior_generation/euslisp/torque-gradient/torque-grad-test.lisp
+++ contact_behavior_generation/euslisp/torque-gradient/torque-grad-test.lisp
@@ -3,11 +3,12 @@
 (require "human-ball-test.lisp")
 (require "rotational-6dof.lisp")
 
-;; (test-sphere-human-ball :key-list '(:rleg :lleg :rarm :larm) :debug-view :no-message :gain1 0.530954 :gain2 -0.01 :rest-torque-ik-args (list :contact-wrench-optimize? nil))
+;; (test-sphere-human-ball :key-list '(:rleg :lleg :rarm :larm) :stop 50 :debug-view :no-message :null-max 0.3 :gain1 0.01 :gain2 0.01 :rest-torque-ik-args (list :contact-wrench-optimize? t) :human-ball-pose-args (list :human-ball-init-pose '(progn (reset-pose) (send *robot* :newcoords (make-coords :pos (float-vector 0 0 -650))))))
 
 ;; (test-sphere-human-ball-loop :key-list '(:rleg :lleg :rarm :larm) :stop 50 :debug-view nil :gain1 0.530954 :gain2 0.01 :rest-torque-ik-args (list :contact-wrench-optimize? t) :human-ball-pose-args (list :human-ball-init-pose '(progn (reset-pose) (send (car (send *robot* :links)) :newcoords (make-coords :pos (float-vector 0 0 -650))))))
 
 (setq
  *test-human-ball-rsd*
- (test-sphere-human-ball-loop :key-list '(:rleg :lleg :rarm :larm) :stop 50 :null-max 0.5 :debug-view nil :gain1 nil :gain2 nil :rest-torque-ik-args (list :contact-wrench-optimize? t) :human-ball-pose-args (list :human-ball-init-pose '(progn (reset-pose) (send (car (send *robot* :links)) :newcoords (make-coords :pos (float-vector 0 0 -650)))))))
+ (test-sphere-human-ball-loop :key-list '(:rleg :lleg :rarm :larm) :stop 50 :null-max 0.3 :debug-view nil :gain1 0.01 :gain2 0.01 :rest-torque-ik-args (list :contact-wrench-optimize? t) :human-ball-pose-args (list :human-ball-init-pose '(progn (reset-pose) (send *robot* :newcoords (make-coords :pos (float-vector 0 0 -650)))))))
 
+;; (dump-loadable-structure "log.test-human-ball.rsd" *test-human-ball-rsd*)
