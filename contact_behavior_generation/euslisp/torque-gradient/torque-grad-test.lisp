abd66c3ad06297212f88136f50a1269b1e2d0e52
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-10-24 16:27:10 +0900

    def debug function

--- contact_behavior_generation/euslisp/torque-gradient/torque-grad-test.lisp
+++ contact_behavior_generation/euslisp/torque-gradient/torque-grad-test.lisp
@@ -9,6 +9,24 @@
 
 (setq
  *test-human-ball-rsd*
- (test-sphere-human-ball-loop :key-list '(:rleg :lleg :rarm :larm) :stop 50 :null-max 0.3 :debug-view nil :gain1 0.01 :gain2 0.01 :rest-torque-ik-args (list :contact-wrench-optimize? t) :human-ball-pose-args (list :human-ball-init-pose '(progn (reset-pose) (send *robot* :newcoords (make-coords :pos (float-vector 0 0 -650)))))))
+ (test-sphere-human-ball-loop :key-list '(:rleg :lleg :rarm :larm) :stop 55 :null-max 0.5 :debug-view nil :gain1 0.05 :gain2 0.01 :rest-torque-ik-args (list :contact-wrench-optimize? t) :human-ball-pose-args (list :human-ball-init-pose '(progn (reset-pose) (send *robot* :newcoords (make-coords :pos (float-vector 0 0 -650)))))))
 
-;; (dump-loadable-structure "log.test-human-ball.rsd" *test-human-ball-rsd*)
+(dump-loadable-structure "log.test-human-ball.rsd" *test-human-ball-rsd*)
+
+(defun parse-from-key
+  (&optional (key :f))
+  (mapcar
+   #'(lambda (dl) (send-all dl :buf key))
+   (cdr *test-human-ball-rsd*)))
+
+(defun average-from-key
+  (&optional (key :f))
+  (let* ((ret (instantiate float-vector 3))
+	 (dll (parse-from-key key))
+	 (len (length dll)))
+    (dolist (dl dll)
+      (dotimes (i 3)
+	(setf (aref ret i) (+ (aref ret i) (nth i dl)))))
+    (dotimes (i 3)
+      (setf (aref ret i) (/ (aref ret i) len)))
+    ret))
