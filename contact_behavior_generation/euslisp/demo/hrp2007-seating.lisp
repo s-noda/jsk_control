ece58eb358810e5e6c92b3ba95a6ca5f5ed2db7c
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-09 03:42:50 +0900

    add hrp2007-seating.lisp

---
+++ contact_behavior_generation/euslisp/demo/hrp2007-seating.lisp
@@ -0,0 +1,65 @@
+#!/usr/bin/env roseus
+
+(defvar *robot-type* :hrp2jsk-collada)
+(require "package://contact_behavior_generation/euslisp/motion-sequencer.lisp")
+(require "package://contact_behavior_generation/euslisp/dynamic-connector.lisp")
+(require "package://contact_behavior_generation/euslisp/motion-planners/motion-planner.lisp")
+
+;; @Override
+(defun demo-motion-sequence (&rest args) (let* ((ret (apply 'demo-motion-sequence2 (append args (list :error-thre 1.1))))) (cons (car ret) (reverse (cdr ret)))))
+
+(defun test-proc
+  (key)
+  (cond
+   ((let* ((init
+            (progn (demo-climb-setup :four-leg-seat)
+                   ;;
+                   (dolist (v (send-all (send-all *contact-states* :target-coords) :worldpos))
+                     (setf (aref v 2) (- (aref v 2) 10)))
+                   ;;
+                   (setq *goal-contact-state* (nth 86 *contact-states*))
+                   ;; (send *robot* :reset-manip-pose)
+                   (send *robot* :reset-pose)
+                   (send *robot* :arms :elbow-p :joint-angle -90)
+                   (send *robot* :fix-leg-to-coords (make-coords))))
+           (rsd (demo-motion-sequence-with-timer
+                 :ik-debug-view *ik-debug-view*
+                 :loop-max 2
+                 :remove-limb :hip
+                 :all-limbs '(:hip)
+                 :now-rsd
+                 ;;(instance robot-state-data2 :init :contact-states (cons (now-hip-contact-state) (now-contact-state :limb-keys '(:rleg :lleg))))
+                 (optimize-brli :contact-states
+                                (now-contact-state :limb-keys '(:rleg :lleg))
+                                :rest-contact-states
+                                (list (now-hip-contact-state)))
+                 :tmax-hand-rate 1.0
+                 :tmax-leg-rate 1.0
+                 :log-file (format nil "~A/four-leg-seat" *log-root*)
+                 )))
+      (setq *rsd* rsd)
+      (cond
+       ((listp rsd)
+        (rsd-play :file (format nil "~A/four-leg-seat.rsd" *log-root*) :auto? t)
+        (quit-graph)))
+      (atom rsd))
+    (print 'four-leg-seat-error)
+    t)))
+
+;; start test
+
+(defvar *log-root* (format nil "log/~A" (log-surfix)))
+(defvar *ik-debug-view* nil) ;;:no-message)
+(unix:system (format nil "mkdir -p ~A" *log-root*))
+
+(if (test-proc :four-leg-seat)
+    (warning-message 1 "not solutions found~%"))
+
+#|
+
+(require "package://hrpsys_ros_bridge_tutorials/euslisp/hrp2jsk-interface.l")
+(hrp2jsk-init)
+
+(send (nth 0 (reverse *rsd*)) :draw :friction-cone? nil :torque-draw? nil)
+
+(model2real)
