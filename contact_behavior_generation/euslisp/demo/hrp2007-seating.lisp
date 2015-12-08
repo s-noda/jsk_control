8342c69a156da800d833a3f5457069952cf99638
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-12-09 04:12:38 +0900

    add exp comments

--- contact_behavior_generation/euslisp/demo/hrp2007-seating.lisp
+++ contact_behavior_generation/euslisp/demo/hrp2007-seating.lisp
@@ -17,10 +17,11 @@
                    (dolist (v (send-all (send-all *contact-states* :target-coords) :worldpos))
                      (setf (aref v 2) (- (aref v 2) 10)))
                    ;;
-                   (setq *goal-contact-state* (nth 86 *contact-states*))
-                   ;; (send *robot* :reset-manip-pose)
-                   (send *robot* :reset-pose)
-                   (send *robot* :arms :elbow-p :joint-angle -90)
+                   ;; (setq *goal-contact-state* (nth 86 *contact-states*))
+                   (setq *goal-contact-state* (nth 87 *contact-states*))
+                   (send *robot* :reset-manip-pose)
+                   ;; (send *robot* :reset-pose)
+                   ;; (send *robot* :arms :elbow-p :joint-angle -90)
                    (send *robot* :fix-leg-to-coords (make-coords))))
            (rsd (demo-motion-sequence-with-timer
                  :ik-debug-view *ik-debug-view*
@@ -62,4 +63,20 @@
 
 (send (nth 0 (reverse *rsd*)) :draw :friction-cone? nil :torque-draw? nil)
 
-(model2real)
+;; init
+(send (nth 2 (reverse *rsd*)) :draw :friction-cone? nil :torque-draw? nil)
+(model2real :sleep-time 10000)
+;; squat
+(send (nth 4 (reverse *rsd*)) :draw :friction-cone? nil :torque-draw? nil)
+(model2real :sleep-time 10000)
+;; straight
+(send (nth 5 (reverse *rsd*)) :draw :friction-cone? nil :torque-draw? nil)
+(model2real :sleep-time 10000)
+;; slide
+(send (nth 6 (reverse *rsd*)) :draw :friction-cone? nil :torque-draw? nil)
+(model2real :sleep-time 20000)
+;; last
+(send-all (send *robot* :torso :joint-list) :joint-angle 0)
+(send-all (send *robot* :head :joint-list) :joint-angle 0)
+(send *viewer* :draw-objects)
+(model2real :sleep-time 10000)
